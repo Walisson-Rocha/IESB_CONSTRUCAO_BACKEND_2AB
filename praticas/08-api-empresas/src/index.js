74e9e95035b1b33ee496b5b4f9957ef95c7c79d9
Walisson <walissonoficial19@gmail.com>
2025-11-07 20:31:40 -0300

    index

--- praticas/08-api-empresas/src/index.js
+++ praticas/08-api-empresas/src/index.js
@@ -1,37 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // conexão com o banco
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASS = process.env.DB_PASS
-const DB_NAME = process.env.DB_NAME
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(url)
   .then(() => {
-    console.log("Conectado ao banco MongoDB!!!!")
+    console.log("✅ Conectado ao banco MongoDB!!!!");
   })
   .catch(erro => {
-    console.log("Erro ao conectar no banco MongoDB: ", erro)
-  })
+    console.log("❌ Erro ao conectar no banco MongoDB: ", erro);
+  });
 
-// rotas
-const CargoController = require('./controllers/CargoController')
-app.use(CargoController)
+// Rotas principais (Controllers)
+const DepartamentoController = require('./controllers/DepartamentoController');
+const CargoController = require('./controllers/CargoController');
+const FuncionarioController = require('./controllers/FuncionarioController');
+const ProjetoController = require('./controllers/ProjetoController');
+const TarefaController = require('./controllers/TarefaController');
 
-const DepartamentoController = require('./controllers/DepartamentoController')
-app.use(DepartamentoController)
+// Usa cada controller com seu prefixo de rota
+app.use('/departamentos', DepartamentoController);
+app.use('/cargos', CargoController);
+app.use('/funcionarios', FuncionarioController);
+app.use('/projetos', ProjetoController);
+app.use('/tarefas', TarefaController);
 
-const FuncionarioController = require('./controllers/FuncionarioController')
-app.use(FuncionarioController)
+// Rota inicial para teste
+app.get('/', (req, res) => {
+  res.json({ mensagem: '🚀 API de Empresas rodando com sucesso!' });
+});
 
 app.listen(3000, () => {
-  console.log("Aplicação rodando -> http://locahost:3000")
-})
+  console.log("Servidor rodando -> http://localhost:3000");
+});
